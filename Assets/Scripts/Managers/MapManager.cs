813e150ac0b623c788015f58783df33b884ee304
s8my <mahmuturdun@hotmail.com>
2022-10-17 17:27:51 +0300

    Heatmap color visual added

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -13,22 +13,9 @@ public class MapManager : MonoBehaviour
         {
             GameObject cityObject = Instantiate<GameObject>(city.cityPrefab.transform.gameObject);
             cityObject.transform.parent = gameObject.transform;
-            cityObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            float colorChanger = Random.Range(0,1f);
+            //cityObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            cityObject.GetComponent<MeshRenderer>().material.color= new Color(2.0f * colorChanger, 2.0f * (1 - colorChanger), 0);
         }
     }
-
-    void Update() 
-    {
-        /*Transform selectedCity = CityManager.instance.GetSelectedCity();
-        Debug.Log(selectedCity.name);
-
-        if(selectedCity !=null)
-        {
-            //selectedCity.localScale = Vector3.Lerp(selectedCity.localScale,new Vector3(1.1f,0,1.1f),10f*Time.deltaTime);
-            selectedCity.position.y = selectedCity.position.;
-        }
-
-        //Debug.Log(GetMouseHitTransform(cityLayerMask));*/
-    }
-    
 }
--- Assets/Scripts/Mouse/MouseController.cs
+++ Assets/Scripts/Mouse/MouseController.cs
@@ -6,11 +6,11 @@ public class MouseController : MonoBehaviour
 {
     void OnMouseEnter()
     {
-        transform.localPosition = new Vector3(transform.position.x,transform.position.y+0.2f,transform.position.z);
+        transform.localPosition = new Vector3(transform.position.x,transform.position.y+0.2f,transform.position.z-0.05f);
         CityManager.instance.SetSelectedCity(gameObject.transform);
     }
     private void OnMouseExit()
     {
-        transform.localPosition = new Vector3(transform.position.x,transform.position.y-0.2f,transform.position.z);
+       transform.localPosition = new Vector3(transform.position.x,transform.position.y-0.2f,transform.position.z+0.05f); 
     }
 }

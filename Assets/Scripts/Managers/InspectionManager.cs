47be1c42f26ea4540caa8501d5f41db3f7f32a30
s8my <mahmuturdun@hotmail.com>
2022-10-29 22:07:23 +0300

    Inspect Scene Proprty Tab

--- Assets/Scripts/Managers/InspectionManager.cs
+++ Assets/Scripts/Managers/InspectionManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InspectionManager : MonoBehaviour
 {
     [SerializeField] Transform cityPrefabLocation;
     [SerializeField] Transform parentObject;
+    [SerializeField] TextMeshProUGUI cityNameText;
+    [SerializeField] TextMeshProUGUI cityPlateNumText;
+    [SerializeField] TextMeshProUGUI cityHeatValueText;
+    [SerializeField] TextMeshProUGUI cityPopValText;
     GameObject cityObject = null;
+    CitySO selectedCity;
 
     void OnEnable()
     {
         cityObject = Instantiate<GameObject>(CityManager.instance.GetSelectedCity().gameObject,new Vector3(cityPrefabLocation.transform.position.x,cityPrefabLocation.transform.position.y+1f,cityPrefabLocation.transform.position.z),Quaternion.identity);
         cityObject.GetComponent<CityMouseController>().enabled = false; 
         cityObject.transform.rotation = Quaternion.AngleAxis(45, Vector3.right);
+        selectedCity = cityObject.GetComponent<City>().citySO;
+        cityNameText.text = "Şehir Adı: "+selectedCity.cityData.cityName;
+        cityPlateNumText.text = "Plaka No: "+selectedCity.cityData.plateNo.ToString();
+        cityHeatValueText.text = "Sıcaklık Değeri: "+selectedCity.cityData.heatValue.ToString();
+        cityPopValText.text = "Nüfus: "+selectedCity.cityData.populationValue.ToString();
+
     }
     void OnDisable() 
     {
